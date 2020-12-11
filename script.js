09c0531e42a1f8c003a6ac5d5bbbda51681b54ce
Anthony Gannuscio <m3t4ldood@gmail.com>
2020-12-10 22:28:57 -0800

    Update script.js
    scripted

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 var resultEl = document.querySelector('#password');
 var generate = document.querySelector('#generate');
 var copyClip = document.querySelector('#copyClip');
